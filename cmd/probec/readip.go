1fc3bb65a094fe3b839bb63ac736b898db3ec3a8
e3930186 <512504317@qq.com>
2016-07-18 14:24:46 +0800

    检测重复IP

--- cmd/probec/readip.go
+++ cmd/probec/readip.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-//	"bufio"
+	//	"bufio"
 	"net"
-//	"os"
-  "fmt"
+	//	"os"
+	"fmt"
 )
 
 var srcIP []string
@@ -22,24 +22,25 @@ func readIPFile() (src []string, err error) { //从ip.txt中读取信息
 
 	//	}
 	//	defer f.Close()
+	chack := make(map[string]int)
 	addrs, errs := net.InterfaceAddrs()
-
 	if errs != nil {
 		fmt.Println(errs)
 		err = errs
 		return
 	}
-//	fmt.Println(time.Now())
-	for _, address := range addrs {
+
+	for i, address := range addrs {
 
 		// 检查ip地址判断是否回环地址
 		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
 			if ipnet.IP.To4() != nil {
-				//fmt.Println(ipnet.IP.String())
-				src = append(src, ipnet.IP.String())
+				chack[ipnet.IP.String()] = i
 			}
-
 		}
+	}
+	for s, _ := range chack {
+		src = append(src, s)
 	}
 	return
 }
